3662b2775e0532616ab5f5ce91b13d8b53d1837f
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-01 23:35:23 +0530

    04_DRIVER.sv

--- D_FLIP_FLOP_VERIFICATION/04_DRIVER.sv
+++ D_FLIP_FLOP_VERIFICATION/04_DRIVER.sv
@@ -2,6 +2,8 @@ class driver;
   virtual intf vinf;
   transaction trans;
   mailbox mb_gen;
+  event next;
+
   
   function new(virtual intf vinf, mailbox mb_gen);
     this.vinf = vinf;
@@ -11,13 +13,12 @@ class driver;
   task start();
     repeat(5)
       begin
-        @(posedge vinf.clk)
-        #2;
-        mb_gen.get(trans);
+	      
+	mb_gen.get(trans);
         vinf.rst 	= 	trans.rst;
-        vinf.d		=	  trans.d;
-        trans.display("driver");
-	//	#4;
+        vinf.d		=	trans.d;
+        trans.display("DRV");  
+	      
       end
   endtask
 endclass
