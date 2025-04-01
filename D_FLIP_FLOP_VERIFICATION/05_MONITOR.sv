8d9303d0227b3f9a33d1a27fb1182ccd47ab70f4
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-01 23:34:41 +0530

    05_MONITOR.sv

--- D_FLIP_FLOP_VERIFICATION/05_MONITOR.sv
+++ D_FLIP_FLOP_VERIFICATION/05_MONITOR.sv
@@ -2,26 +2,28 @@ class monitor;
   virtual intf vinf;
   transaction trans;
   mailbox mb_mtr;
-  
+    event next;
+
   function new(virtual intf vinf, mailbox mb_mtr);
     this.mb_mtr = mb_mtr;
     this.vinf = vinf;
   endfunction
   
-  task start();
+  task start();	
     repeat(5)
-		begin
+	begin
+	trans = new();
           @(posedge vinf.clk)
-          #3;
-          	trans = new();
-			      trans.rst	=   vinf.rst; 	
-			      trans.d  	=	  vinf.d;
-			      trans.q  	= 	vinf.q; 
-          	
+   		  #1;         
+			trans.rst	=  	vinf.rst; 	
+			trans.d  	=	vinf.d;
+			trans.q  	= 	vinf.q;
           mb_mtr.put(trans);
-          trans.display("monitor");
-
+          trans.display("MON");
+          
         end
   endtask
   
 endclass
+    
+        
