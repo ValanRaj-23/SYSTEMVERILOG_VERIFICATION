ec324dcbf3386c0ec8f0ae65a63d2c3ad446a712
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-01 23:33:08 +0530

    06_SCORE_BOARD.sv

--- D_FLIP_FLOP_VERIFICATION/06_SCORE_BOARD.sv
+++ D_FLIP_FLOP_VERIFICATION/06_SCORE_BOARD.sv
@@ -1,31 +1,31 @@
 class scoreboard;
   transaction trans;
   mailbox mb_mon;
+  event next;
+  
   
   function new(mailbox mb_mon);
     this.mb_mon = mb_mon;
   endfunction
   
   task start();
+    trans = new();
     repeat(5)
       begin
-        mb_mon.get(trans);
-
+		mb_mon.get(trans);
+        trans.display("SBD");
+        
         if((trans.rst == 0) && (trans.q == 0))
-          	$display("test case passed");
+          $display("\t    test case passed");
         else if((trans.rst == 1) && (trans.q == trans.d))
-         	$display("test case passed");
+          $display("\t    test case passed");
         else
-          $display("test case failed");
-          $display("score board");
+          $display("\t    test case failed");
         
-       
+        trans.display_line();
+        -> next;
       end
+		
   endtask
-endclass           
-        
-        
-    
-    
-  
+endclass  
   
