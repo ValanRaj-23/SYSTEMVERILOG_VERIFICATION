9ba3599192d11b48b45f5d8e325268f97267f128
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-01 23:36:21 +0530

    02_TRANSACTION.sv

--- D_FLIP_FLOP_VERIFICATION/02_TRANSACTION.sv
+++ D_FLIP_FLOP_VERIFICATION/02_TRANSACTION.sv
@@ -12,16 +12,16 @@ class transaction;
   }
   
   function void display(string dis);
-    $display(" %s ", dis);
-    $display("rst = %0b d = %0b q = %0b", rst, d, q);
-    $display(" count = %0d", count);
+    $display(" %0t [%s] count = %0d rst = %0b d = %0b q = %0b",$time, dis, count, rst, d, q);
   endfunction
   
+    function void display_line();
+    $display("________________________________________");
+  	endfunction
   
   function void check();
     count++;
   endfunction
-  
   
 endclass
 
