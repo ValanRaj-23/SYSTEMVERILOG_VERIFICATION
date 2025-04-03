e71ec7436c5b84a6ae327c30a8204db397c34793
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-03 20:11:13 +0530

    05_MONITOR.sv

--- VERIFICATION_SRAM/05_MONITOR.sv
+++ VERIFICATION_SRAM/05_MONITOR.sv
@@ -1,24 +1,24 @@
-class monitor;
+class monitor #(int times = 10);
 	virtual intf vinf;
   	transaction trans;
   	mailbox mb_drv;
   
-  function new( mailbox mb_drv, virtual intf vinf);
+  function new( virtual intf vinf, mailbox mb_drv);
     this.vinf 	= vinf;
     this.mb_drv	= mb_drv;
   endfunction
   
   task start();
-    repeat(5)
+    repeat(times)
       begin
         trans = new();
         
         @(posedge vinf.clk)
-        #1;
+		#1;        
         trans.data_in 	= vinf.data_in;
-        trans.addr 		= vinf.addr;
-        trans.en		= vinf.en;
-        trans.wr		= vinf.wr;
+        trans.addr 	= vinf.addr;
+        trans.en	= vinf.en;
+        trans.wr	= vinf.wr;
         trans.data_out	= vinf.data_out;
         
         mb_drv.put(trans);
