73153239d27428d1399984feeb1fdd40146a905a
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-03 20:16:09 +0530

    Update 09_TEST_BENCH.sv

--- VERIFICATION_SRAM/09_TEST_BENCH.sv
+++ VERIFICATION_SRAM/09_TEST_BENCH.sv
@@ -1,1 +1,39 @@
+`include "interface.sv"
+`include "test.sv"
+
+module tb;
+  
+  bit clk;
+ 
+  always #5 clk = ~clk;  
+  intf inf(clk);
+  sram uut(inf);
+  test tst;
+
+  initial begin
+    tst = new(inf); 
+  	#5;
+    inf.rst = 0;
+    #15;
+    inf.rst = 1;
+    inf.wr 	= 1;
+    inf.en 	= 1;
+    
+    #180;
+    inf.wr 	= 0;
+    inf.en 	= 1;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    #500;
+    $finish;
+  end
+  
+  initial begin
+      tst.run();
+  end
+  
+endmodule
 
