d326d6e41ae81300c58d155748a948f24e7a1e79
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-03 20:11:51 +0530

    Update 04_DRIVER.sv

--- VERIFICATION_SRAM/04_DRIVER.sv
+++ VERIFICATION_SRAM/04_DRIVER.sv
@@ -1,25 +1,24 @@
-class driver;
+class driver #(int times = 10);
 	transaction trans;
   	virtual intf vinf;
   	mailbox mb_gen;
   
-  function new( mailbox mb_gen, virtual intf vinf);
+  function new( virtual intf vinf, mailbox mb_gen);
     this.vinf 	= vinf;
     this.mb_gen	= mb_gen;
   endfunction
   
   task start();
-    repeat(5);
+    repeat(times)
     begin
       
     mb_gen.get(trans);
       
+   @(posedge vinf.clk)
+      
     vinf.data_in 	= trans.data_in;
     vinf.addr		= trans.addr;
-    vinf.en			= trans.en;
-    vinf.wr			= trans.wr;
-//     vinf.data_out	= trans.data_out;
-      
+	    
     trans.display("DRV");
       
     end
