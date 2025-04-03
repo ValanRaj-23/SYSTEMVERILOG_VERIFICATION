3b14872fde973c6d2654e113eba5752cfc9e6da5
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-03 20:16:54 +0530

    Update 00_DESIGN.sv

--- VERIFICATION_SRAM/00_DESIGN.sv
+++ VERIFICATION_SRAM/00_DESIGN.sv
@@ -1,24 +1,18 @@
-module sram(intf.ds inf);
+module sram(intf inf);
 
   reg [3:0] mem [0:15];
 
   always@(posedge inf.clk)
 	begin
-      if(!inf.rst)
-        begin
+      	if(!inf.rst)
         foreach(mem[i])
           begin
             mem[i] <= 4'h0;
           end
-        end
-      else
-        begin
-  		if(inf.en && inf.wr)
+      	else if(inf.en && inf.wr)
   		  	mem[inf.addr] <= inf.data_in;
   		else if (inf.en && !inf.wr)
 			inf.data_out <= mem[inf.addr];
-        end
 	end
 
 endmodule
-
