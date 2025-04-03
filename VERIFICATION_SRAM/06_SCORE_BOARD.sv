1324961ca2f552aeafc3e6e4b6e7bfa8372d75e4
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-03 20:10:41 +0530

    Update 06_SCORE_BOARD.sv

--- VERIFICATION_SRAM/06_SCORE_BOARD.sv
+++ VERIFICATION_SRAM/06_SCORE_BOARD.sv
@@ -1,7 +1,7 @@
-class scoreboard;
+class scoreboard #(int times = 10);
   	transaction trans;
   	mailbox mb_drv;
-  	bit [3:0] temp [16];  // 16 locations for memory storage
+  	bit [3:0] temp [16];  
   	bit [3:0] temp_data;
   	event next;
   
@@ -10,35 +10,40 @@ class scoreboard;
   endfunction
   
   task start();
-  // Initialize transaction object
-    repeat(5) begin
+    repeat(times)
+      begin
       mb_drv.get(trans);
-      
-      if (trans.en && trans.wr) begin
-        if (trans.addr < 16) begin
-          temp[trans.addr] <= trans.data_in;
-          $display(" DATA IS STORED at Address: %0d, Data: %0d", trans.addr, trans.data_in);
-        end else begin
-          $display("ERROR: Address out of range: %0d", trans.addr);
-        end
-      end
-      
-      else if (trans.en && !trans.wr) begin
-        if (trans.addr < 16) begin
+		trans.display("SCB");
+
+      if (trans.en && trans.wr)
+        write();
+      else if (trans.en && !trans.wr) 
+        read();
+        
+      -> next; 
+    end
+  endtask  
+  
+  task write();
+     if (trans.addr < 16) 
+            begin
+          		temp[trans.addr] <= trans.data_in;
+              $display(" DATA IS STORED ");
+        end 
+  endtask
+  
+  task read();
+    if (trans.addr < 16) begin
           temp_data = temp[trans.addr];
           
           if (temp_data == trans.data_out) begin
-            $display(" DATA IS MATCHED at Address: %0d, Expected: %0d, Got: %0d", trans.addr, temp_data, trans.data_out);
-          end else begin
-            $display(" DATA IS NOT MATCHED at Address: %0d, Expected: %0d, Got: %0d", trans.addr, temp_data, trans.data_out);
+            $display("DATA IS MATCHED at Address: %0d, Expected: %0d, Got: %0d", trans.addr, temp_data, trans.data_out);
+          end 
+      	else begin
+            $display("DATA IS NOT MATCHED at Address: %0d, Expected: %0d, Got: %0d", trans.addr, temp_data, trans.data_out);
           end
-        end else begin
-          $display("ERROR: Read Address out of range: %0d", trans.addr);
-        end
-      end
-      
-      -> next; // Trigger next event
-    end
+        end 
   endtask
+    
 
 endclass
