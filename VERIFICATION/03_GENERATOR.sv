92e5db08d60ffdef06f7cfb609252505158306ac
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-04-09 23:16:54 +0530

    Create 03_GENERATOR.sv

---
+++ VERIFICATION/03_GENERATOR.sv
@@ -0,0 +1,25 @@
+class generator #(parameter count = 10);
+  transaction trans;
+  mailbox mb_gen;
+  event move;
+  
+  function new(mailbox mb_gen);
+  this.mb_gen = mb_gen;
+  endfunction
+  
+  	task start();
+		repeat(count)
+	  	begin
+          
+					trans = new();
+					void'(trans.randomize());
+					mb_gen.put(trans);
+          
+          trans.display_line();
+					trans.display("GEN");
+					@(move);
+		
+        end
+	endtask
+  
+endclass
